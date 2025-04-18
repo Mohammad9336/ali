847920beb300ebfed2e3ad8ea0f11bd79ddde367
Mohammad9336 <mavaddathossien@gmail.com>
2025-04-18 17:22:08 +0330

    second commit

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
     std::cout << "Hello World!\n";
+    int studentnumber;
+    cin >> studentnumber;
+    cout << studentnumber << endl;
 }
